06303c31942ea93cf8c437b9162b4934eb004298
lululalei64 <jjjjing12@gmail.com>
2025-12-12 10:59:55 +0800

    modify fs/stat.c to compatible w manual hook v1.6

--- fs/stat.c
+++ fs/stat.c
@@ -354,6 +354,7 @@ SYSCALL_DEFINE2(newlstat, const char __user *, filename,
 }
 
 #ifdef CONFIG_KSU
+__attribute__((hot)) 
 extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
 #endif
 #if !defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_SYS_NEWFSTATAT)
